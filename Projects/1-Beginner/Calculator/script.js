1aacd067447f4d1d250cb530d927b33e01923570
Lucas Conceição dos Santos <lucas.cds1997@gmail.com>
2021-11-13 01:46:12 -0300

    add visual css

---
+++ Projects/1-Beginner/Calculator/script.js
@@ -0,0 +1,11 @@
+/* 
+
+Etapas para calculadora
+
+1 - Fazer cada botão coletar seu valor devido;
+2 - Adicionar valores ao result em um array a partir do click;
+3 - Adicionar calculo em relação ao botão selecionado;
+4 - Calculo resultado;
+
+*/
+
