15d028daf252c5dc414fb038210cd5a7139bda60
Lucas Conceição dos Santos <lucas.cds1997@gmail.com>
2021-11-20 01:51:00 -0300

    getting values and push to array

--- Projects/1-Beginner/Calculator/script.js
+++ Projects/1-Beginner/Calculator/script.js
@@ -2,10 +2,19 @@
 
 Etapas para calculadora
 
-1 - Fazer cada botão coletar seu valor devido;
-2 - Adicionar valores ao result em um array a partir do click;
-3 - Adicionar calculo em relação ao botão selecionado;
-4 - Calculo resultado;
+1 - Fazer cada botão coletar seu valor devido - OK
+2 - Adicionar valores ao result em um array a partir do click - OK
+3 - Separar valor do result a partir do momento que clica em algum operador
+4 - Adicionar calculo em relação ao botão selecionado;
+5 - Calculo resultado;
 
 */
 
+result = []
+
+function getInputValue(val){
+    let valor = val.value
+    result.push(valor)
+    console.log(result)
+    
+}
